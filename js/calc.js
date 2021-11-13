822e7e47755746127ca2338d274e18ff88fb7a7f
Ivan Gricyan <ivan_gricyan82@ukr.net>
2021-11-13 15:42:43 +0100

    feat: add buttons eventListener, lesson 10

---
+++ js/calc.js
@@ -0,0 +1,26 @@
+/* examples
+var btn = document.getElementById('ce');
+var display = document.getElementById('display');
+
+btn.addEventListener('click', function (e){
+  alert('CE button');
+});
+
+display.addEventListener('keyup', function (e){
+  alert(e.target.value);
+});
+*/
+
+var btns = document.querySelectorAll('.btn');
+var display = document.getElementById('display');
+
+var handleClick = function (e) {
+  var value = e.target.textContent;
+  display.value  = value;
+}
+
+for (var i = 0; i < btns.length; i++) {
+  var btn = btns[i];
+
+  btn.addEventListener('click', handleClick);
+};
